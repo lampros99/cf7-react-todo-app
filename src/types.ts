1549c28d8e4e624a1b32357831c3d771d322647d
lampros alexandris <lamprospatmos19@gmail.com>
2025-06-08 06:37:33 +0300

    refactor(types): move todo, action

---
+++ src/types.ts
@@ -0,0 +1,20 @@
+export type TodoProps = {
+    id: number;
+    text:string;
+}
+
+export type Action =
+| {type: "ADD"; payload: string}
+| {type: "DELETE"; payload: number}
+| {type: "EDIT"; payload: {id: number; newText: string} }
+
+
+export type TodoFromProps = {
+    dispatch: React.Dispatch<Action>;
+}
+
+
+export type TodoListProps = {
+    todos: TodoProps[];
+    dispatch: React.Dispatch<Action>
+}
